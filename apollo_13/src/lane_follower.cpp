5d7eef83ac817c1a6ecb200efee315616a11889b
nsteel <aceronicolas@hotmail.com>
2016-08-03 15:06:34 +0200

    New callback to control the steering without the navigation stack

--- apollo_13/src/lane_follower.cpp
+++ apollo_13/src/lane_follower.cpp
@@ -25,27 +25,44 @@
 #include <lane_detector/Lane.h>
 #include <nav_msgs/Path.h>
 #include <tf/transform_listener.h>
+#include<std_msgs/Int32.h>
 
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
-//cv_bridge::CvImageConstPtr currentFrame_ptr;
 ros::Publisher obstacles_pub;
 ros::Publisher path_pub;
+ros::Publisher motorControl_pub;
+ros::Publisher steeringControl_pub;
 
-/*{
+const double angleArray[101]={25.0, 24.6, 24.2, 23.8, 23.4, 23.0, 22.6, 22.2, 21.8, 21.4,
+						21.0, 20.6, 20.2, 19.8, 19.4, 19.0, 18.6, 18.2, 17.8, 17.4,
+						17.0, 16.5, 16.0, 15.5, 15.0, 14.5, 14.0, 13.5, 13.0, 12.5,
+						12.0, 11.7, 11.4, 11.1, 10.8, 10.5, 10.2, 9.9, 9.6, 9.3,
+						9.0, 8.7, 8.4, 8.1, 7.8, 7.5, 7.2, 6.9, 6.6, 6.3,
+						6.0, 5.2, 4.4, 3.6, 2.8, 2.0, 1.2, 0.4, -0.4, -1.2,
+						-2.0, -2.3, -2.6, -2.9, -3.2, -3.5, -3.8, -4.1, -4.4, -4.7,
+						-5.0, -5.4, -5.8, -6.2, -6.6, -7.0, -7.4, -7.8, -8.2, -8.6,
+						-9.0, -9.4, -9.8, -10.2, -10.6, -11.0, -11.4, -11.8, -12.2, -12.6,
+						-13.0, -13.5, -14.0, -14.5, -15.0, -15.5, -16.0, -16.5, -17.0, -17.5, -18.0};
 
-  try
-  {
-    currentFrame_ptr = cv_bridge::toCvShare(pointCloud, sensor_msgs::image_encodings::TYPE_16UC1);
-  }
-  catch (cv_bridge::Exception& e)
-  {
-    ROS_ERROR("cv_bridge exception: %s", e.what());
-    return;
-  }
+/* Determines the appropriate steering level to a given steering angle, using
+ * the lookup table from above.
+ */
+int angleToSteering(double alpha){
+    if(alpha>=angleArray[0]){
+      return -50;
+    }else if(alpha<=angleArray[100]){
+      return 50;
+    }else{
+      for(int i = 1; i <= 100; i++){
+        if(alpha < angleArray[i-1] && alpha >= angleArray[i]){
+          return i-50;
+        }
+      }
+    }
+}
 
-}*/
 
 std::vector<geometry_msgs::Point32> splineSampling(const std::vector<geometry_msgs::Point32>& spline) {
 
@@ -149,6 +166,19 @@ void laneCB(const lane_detector::Lane::ConstPtr& lane, tf::TransformListener* li
   }
 }
 
+void laneCB2(const lane_detector::Lane::ConstPtr& lane) {
+  double yaw = std::atan2(lane->guide_line.back().y, lane->guide_line.back().x) * 180/CV_PI;
+  int steering = angleToSteering(yaw);
+  std_msgs::Int32 steering_msg;
+  steering_msg.data = steering;
+  steeringControl_pub.publish(steering_msg);
+  std_msgs::Int32 motor_msg;
+  motor_msg.data = 3;
+  motorControl_pub.publish(motor_msg);
+  ros::spinOnce();
+  ROS_INFO("Steering Angle: %i", steering);
+}
+
 
 int main(int argc, char **argv){
 
@@ -186,9 +216,12 @@ int main(int argc, char **argv){
      MoveBaseClient ac("move_base", true);
 
      //image_transport::Subscriber pointCloud_sub = it.subscribe("camera/depth/image_raw", 1, readPointCloud);
-     ros::Subscriber lane_sub = nh.subscribe<lane_detector::Lane>("/lane_detector/lane", 1, std::bind(laneCB, std::placeholders::_1, &listener, &ac));
+     //ros::Subscriber lane_sub = nh.subscribe<lane_detector::Lane>("/lane_detector/lane", 1, std::bind(laneCB, std::placeholders::_1, &listener, &ac));
+     ros::Subscriber lane_sub = nh.subscribe<lane_detector::Lane>("/lane_detector/lane", 1, std::bind(laneCB2, std::placeholders::_1));
      obstacles_pub = nh.advertise<teb_local_planner::ObstacleMsg>("/move_base/TebLocalPlannerROS/obstacles", 10);
      path_pub = nh.advertise<nav_msgs::Path>("/pathtransformPlanner/path", 10);
+     motorControl_pub = nh.advertise<std_msgs::Int32>("car_handler/motor", 10);
+     steeringControl_pub = nh.advertise<std_msgs::Int32>("car_handler/steering", 10);
      //wait for the action server to come up
      while(!ac.waitForServer(ros::Duration(5.0))){
        ROS_INFO("Waiting for the move_base action server to come up");
